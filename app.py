342237f5c436aa0e4e8808c25f9be21a0ff75b75
gnanasarathy <58853340+gnanasarathy@users.noreply.github.com>
2021-10-06 18:04:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ for i in db_lis:
 
 
 cl_1 = datetime.time(hour=8, minute=58, second=00,tzinfo=pytz.timezone('Asia/Kolkata'))
-cl_2 = datetime.time(hour=10, minute=58, second=00,tzinfo=pytz.timezone('Asia/Kolkata'))
+cl_2 = datetime.time(hour=18, minute=7, second=00,tzinfo=pytz.timezone('Asia/Kolkata'))
 cl_3 = datetime.time(hour=13, minute=58, second=00,tzinfo=pytz.timezone('Asia/Kolkata'))
 
 
@@ -96,6 +96,79 @@ job_daily = job.run_daily(func, days=(4,4), time=cl_1)
 
 def func(context: telegram.ext.CallbackContext):
     context.bot.send_message(chat_id='1090783297', text=links[10])
+
+job_daily = job.run_daily(func, days=(4,4), time=cl_2)
+
+######
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[11])
+
+
+job_daily = job.run_daily(func, days=(0,0), time=cl_1)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[1])
+
+
+job_daily = job.run_daily(func, days=(0,0), time=cl_2)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[2])
+
+
+job_daily = job.run_daily(func, days=(0,0), time=cl_3)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[3])
+
+
+job_daily = job.run_daily(func, days=(1,1), time=cl_1)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[4])
+
+
+job_daily = job.run_daily(func, days=(1,1), time=cl_2)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[5])
+
+
+job_daily = job.run_daily(func, days=(2,2), time=cl_1)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[12])
+
+
+job_daily = job.run_daily(func, days=(2,2), time=cl_2)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[7])
+
+
+job_daily = job.run_daily(func, days=(3,3), time=cl_1)
+
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[8])
+
+job_daily = job.run_daily(func, days=(3,3), time=cl_2)
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[9])
+
+job_daily = job.run_daily(func, days=(4,4), time=cl_1)
+
+def func(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='967948229', text=links[10])
 
 job_daily = job.run_daily(func, days=(4,4), time=cl_2)
 
