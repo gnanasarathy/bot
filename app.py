688006f67a04e69ace22c97fb643b4f0cb920d7c
gnanasarathy <58853340+gnanasarathy@users.noreply.github.com>
2021-10-08 23:56:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,13 @@ dispatcher.add_handler(start_handler)
 
 
 subject = ['ds1', 'el1', 'cs1', 'ci1', 'pcv1', 'ds2', 'ci2', 'cs2', 'pcv2', 'el2']
-users = ['1090783297', '967948229', '1079579102']
-section = ['a', 'a', 'a']
+users = ['1090783297', '967948229', '1079579102', '1054397493']
+section = ['a', 'a', 'a', 'b']
 staff_list = [['jan', 'ui', 'met', 'pd', 'ang', 'jan', 'pd', 'met', 'ang', 'ui'],
               ['abi', 'ui', 'met', 'pd', 'ang', 'abi', 'pd', 'met', 'ang', 'ui'],
-              ['abi', 'ui', 'met', 'pd', 'ang', 'abi', 'pd', 'met', 'ang', 'ui']]
+              ['abi', 'ui', 'met', 'pd', 'ang', 'abi', 'pd', 'met', 'ang', 'ui'],
+              ['abi', 'nlp', 'met', 'pd', 'ang', 'abi', 'pd', 'met', 'ang', 'nlp']]
+
 
 cl_1 = datetime.time(hour=8, minute=58, second=00,tzinfo=pytz.timezone('Asia/Kolkata'))
 cl_2 = datetime.time(hour=10, minute=58, second=00,tzinfo=pytz.timezone('Asia/Kolkata'))
@@ -108,6 +110,32 @@ for t in range(5):
         lab_link = lab[0][0]
         def func(context: telegram.ext.CallbackContext):
             context.bot.send_message(chat_id=users[2], text=lab_link)
+        job_daily = job.run_daily(func, days=day, time=cl[2])
+        f = 0
+    ind += 2
+
+###
+
+f = 1
+query = [subject[q] + '_' + staff_list[3][q] for q in range(len(subject))]
+ind = 0
+for t in range(5):
+    link1 = list(cursor.execute("SELECT link from schedule WHERE class = '{}'".format(query[ind])))
+    link2 = list(cursor.execute("SELECT link from schedule WHERE class = '{}'".format(query[ind+1])))
+    s = "Link for the Class\n"
+    class_t = [s+link1[0][0], s+link2[0][0]]
+    day = (t, t)
+    for x in range(2):
+        def func(context: telegram.ext.CallbackContext):
+            context.bot.send_message(chat_id=users[3], text=class_t[x])
+        job_daily = job.run_daily(func, days=day, time=cl[x])
+
+    if f:
+        lab_q = "lab_" + section[3]
+        lab = list(cursor.execute("SELECT link from schedule WHERE class = '{}'".format(lab_q)))
+        lab_link = lab[0][0]
+        def func(context: telegram.ext.CallbackContext):
+            context.bot.send_message(chat_id=users[3], text=lab_link)
         job_daily = job.run_daily(func, days=day, time=cl[2])
         f = 0
     ind += 2
